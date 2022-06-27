14bb9782c31d069a6fd0dd6c51e941fe1580c79b
unknown <dennisxu54@hotmail.com>
2022-06-27 14:18:31 +1200

    #4 added api call and search box that dynamically search

--- src/Pages/Users/usersPage.jsx
+++ src/Pages/Users/usersPage.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import SearchBox from "../../Components/SearchBox/searchBox";
+import * as qs from "query-string";
+
 const UsersPage = () => {
+  const [userData, setUserData] = useState();
+  const location = useLocation();
+  const { s: search } = qs.parse(location.search);
+
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch(`https://api.github.com/users/${search}`);
+        const data = await res.json();
+        setUserData(data);
+      }
+      catch(error) {
+        console.log(error)
+      }
+    }
+
+    fetchUsers();
+  }, [search])
+
+
     return (
       <>
         <h1>Users Page</h1>
+        <SearchBox />
+        {userData ? <p>{userData.login}</p>: <p>Data is not ready</p>}
       </>
     );
   };
