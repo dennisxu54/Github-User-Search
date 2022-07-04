cf6d69f66bf960d437daccfe00fc3d8e26d04f1f
unknown <dennisxu54@hotmail.com>
2022-07-04 16:01:20 +1200

    #14 added all the properties of a user in github

--- src/Components/SpecificUser/specificUser.jsx
+++ src/Components/SpecificUser/specificUser.jsx
@@ -1,21 +1,55 @@
-import "./specificUser.css"
+import "./specificUser.css";
 
 const SpecificUser = ({ userDetails }) => {
-    const { login, avatar_url, created_at, followers, public_repos } = userDetails;
+  const {
+    login,
+    id,
+    node_id,
+    avatar_url,
+    gravatar_id,
+    url,
+    html_url,
+    followers_url,
+    following_url,
+    gists_url,
+    starred_url,
+    subscriptions_url,
+    organizations_url,
+    repos_url,
+    events_url,
+    received_events_url,
+    type,
+    site_admin,
+    name,
+    company,
+    blog,
+    location,
+    email,
+    hireable,
+    bio,
+    twitter_username,
+    public_repos,
+    public_gists,
+    followers,
+    following,
+    created_at,
+    updated_at
+  } = userDetails;
 
-    return (
+  return (
+    <div className="detail-box">
+      <img src={avatar_url} alt="avatar image" />
       <div className="user-detail">
-        <img src={avatar_url} alt="avatar image" />
-        <div>
-        <b>Name: {login}</b>
+        <b>Name: {name}</b>
         <div>This account was created at: {created_at}</div>
         <div>This account currently has {followers} followers</div>
         <div>This account is currently following {} other users</div>
         <div>This account has {public_repos} Public Repositories</div>
-        </div>
-
       </div>
-    );
-  }
-  
-  export default SpecificUser;
+      <div>
+      </div>
+    </div>
+  );
+};
+
+export default SpecificUser;
