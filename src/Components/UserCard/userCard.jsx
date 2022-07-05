d7fdd78c2f52253e47abd2253032cb62d01d4c22
unknown <dennisxu54@hotmail.com>
2022-07-06 10:15:46 +1200

    #14 tidied up the user cards

--- src/Components/UserCard/userCard.jsx
+++ src/Components/UserCard/userCard.jsx
@@ -1,23 +1,23 @@
 import { Link } from "react-router-dom";
-import "./userCard.css"
+import "./userCard.css";
 
 const UserCard = ({ userDetails }) => {
-  const { login, avatar_url, name, public_repos, followers, created_at, } = userDetails;
+  const { login, avatar_url, public_repos, followers, created_at } =
+    userDetails;
 
-    return (
-      <div className="user-card">
-        <img src={avatar_url} alt="Avatar"></img>
-        <p><b>The username is {login}</b></p>
-        
-        <p>The date of creation of this account was {created_at.slice(0, 10) + " " + created_at.slice(11, -1)}</p>
-        <p>The number of followers are {followers}</p>
-        <p>The number of public repos created are {public_repos}</p>
-        <Link to={`/users/${login}`}>
-          <button>Click</button>
-        </Link>
-        <br></br>
-      </div>
-    );
-  }
-  
-  export default UserCard;
+  return (
+    <Link to={`/users/${login}`} className="user-card">
+      <img src={avatar_url} alt="Avatar"></img>
+      <p>
+        <b>{login}</b>
+      </p>
+      <p>
+        Created at: {created_at.slice(0, 10) + " " + created_at.slice(11, -1)}
+      </p>
+      <p>Followers: {followers}</p>
+      <p>Public repos: {public_repos}</p>
+    </Link>
+  );
+};
+
+export default UserCard;
