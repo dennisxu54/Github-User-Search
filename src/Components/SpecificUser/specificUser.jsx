131ef8b1d2811acea9efc47f91516bd7f6d3a40a
unknown <dennisxu54@hotmail.com>
2022-07-06 13:28:37 +1200

    #14 tidied up the user detail component

--- src/Components/SpecificUser/specificUser.jsx
+++ src/Components/SpecificUser/specificUser.jsx
@@ -23,30 +23,53 @@ const SpecificUser = ({ userDetails }) => {
   return (
     <div className="detail-box">
       <img src={avatar_url} alt="avatar" className="user-detail-img" />
+
       <div>
         <p>
           Login: <b>{login}</b>
         </p>
+        {bio ? <p>Introduction: {bio}</p> : <p>User does not have a Bio</p>}
         <p>
           Github page: <a href={html_url}>{html_url}</a>
         </p>
-        <p>Followers URL: {followers_url}</p>
-        <p>Following: {subscriptions_url}</p>
-        <p>Public Repositories: {organizations_url}</p>
-        <p>Public Repositories: {repos_url}</p>
-        {blog ? <p>User's blog: {blog}</p> : <p>User has no blog</p>}
-      </div>
-      <div>
         <p>
-          Name: <b>{name}</b>
+        Following:{" "}
+          <a href={`https://github.com/${login}?tab=following`}>{following}</a>
         </p>
-        <p>Created at: {bio}</p>
-        <p>Created at: {location}</p>
-        <p>Created at: {created_at}</p>
-        <p>Followers: {followers}</p>
-        <p>Following: {following} users</p>
+        <p>
+          Followers:{" "}
+          <a href={`https://github.com/${login}?tab=followers`}>{followers}</a>
+        </p>
+        <p>
+          Public Repositories:{" "}
+          <a href={`https://github.com/${login}?tab=repositories`}>
+            {public_repos}
+          </a>
+        </p>
+      </div>
+
+      <div>
+        {name !== "undefined" ? (
+          <p>
+            Name: <b>{name}</b>
+          </p>
+        ) : (
+          <p>Name: -----</p>
+        )}
+        {location ? (
+          <p>Country: {location}</p>
+        ) : (
+          <p>User has not entered their Country</p>
+        )}
+        <p>Created at: {created_at.slice(0, 10) + " " + created_at.slice(11, -1)}</p>
         <p>Public Gists: {public_gists}</p>
-        <p>Public Repositories: {public_repos}</p>
+        {blog ? (
+          <p>
+            User's blog: <a href={repos_url}>{blog}</a>
+          </p>
+        ) : (
+          <p>User has no blog website</p>
+        )}
       </div>
     </div>
   );
