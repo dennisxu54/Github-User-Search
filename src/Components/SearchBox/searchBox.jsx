8f70037da7eddf4379a01ea7a461ac0e57275fb6
unknown <dennisxu54@hotmail.com>
2022-07-13 14:52:05 +1200

    #24 prevented default submission of form and used stateful searchQuery

--- src/Components/SearchBox/searchBox.jsx
+++ src/Components/SearchBox/searchBox.jsx
@@ -1,14 +1,24 @@
 import "./searchBox.css";
+import { useNavigate } from "react-router-dom";
 
-const SearchBox = () => {
+const SearchBox = ({ searchQuery, setSearchQuery }) => {
+  let navigate = useNavigate();
+
+  const onSubmit = (e) => {
+    navigate(`?s=${searchQuery}`, { replace: true });
+    e.preventDefault();
+  };
+
   return (
-    <form action="/users" method="get">
+    <form action="/users" method="get" onSubmit={(e) => onSubmit}>
       <input
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
         type="text"
         id="header-search"
         placeholder="Search users"
         name="s"
-      />{" "}
+      />
       <button type="submit" className="search-button">
         Search
       </button>
